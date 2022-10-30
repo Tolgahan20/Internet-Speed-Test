3f3bb02b15c1fdf575e7e1caa3905b187dcdaf3d
Tolgahan Dayanıklı <tolgahan.dayanikli@gmail.com>
2022-10-31 00:38:54 +0100

    Ping Data Added

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 from speedtest import Speedtest
 
 st = Speedtest()
+download = st.download()
+upload = st.upload()
 
-print("----------------------")
+servernames = []
+st.get_servers(servernames)
+ping = st.results.ping
+
 print("YOUR CONNECTION'S ")
 print("----------------------")
-print("DOWNLOAD SPEED: ", st.download())
-print("UPLOAD SPEED: ", st.upload())
+print('DOWNLOAD SPEED: {:.2f} Kb/s\n'.format(download / 1024))
+print('UPLOAD SPEED: {:.2f} Kb/s\n'.format(upload / 1024))
+print('Ping:: {} ms'.format(ping))
